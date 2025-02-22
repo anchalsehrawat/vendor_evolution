29a426ee086ccc906c532bf98361b7e07048a2e8
Joey Huab <jhuab@edukasyon.ph>
2025-02-22 21:53:08 +0900

    config: properties: Feb 2025 pihooks update

--- config/evolution.mk
+++ config/evolution.mk
@@ -2,13 +2,13 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_MODEL?=Pixel 9 Pro Fold \
-    persist.sys.pihooks_FINGERPRINT?=google/comet_beta/comet:Baklava/BP22.250103.008/12932282:user/release-keys \
-    persist.sys.pihooks_PRODUCT?=comet_beta \
-    persist.sys.pihooks_DEVICE?=comet \
-    persist.sys.pihooks_ID?=BP22.250103.008 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-01-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=35
+    persist.sys.pihooks_MODEL?=Pixel 9 \
+    persist.sys.pihooks_FINGERPRINT?=google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys \
+    persist.sys.pihooks_PRODUCT?=tokay_beta \
+    persist.sys.pihooks_DEVICE?=tokay \
+    persist.sys.pihooks_ID?=BP22.250124.009 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-02-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 # Evolution X packages
 PRODUCT_PACKAGES += \
