ec053b0cb5d9a4093abc3fefd068e2d6b8f39a09
Joey Huab <jhuab@edukasyon.ph>
2025-04-19 00:14:13 +0900

    config: Update pihooks to Pixel Android 16.0 Beta 4

--- config/evolution.mk
+++ config/evolution.mk
@@ -2,12 +2,12 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_MODEL?=Pixel 9 \
-    persist.sys.pihooks_FINGERPRINT?=google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys \
-    persist.sys.pihooks_PRODUCT?=tokay_beta \
-    persist.sys.pihooks_DEVICE?=tokay \
-    persist.sys.pihooks_ID?=BP22.250124.009 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-02-05 \
+    persist.sys.pihooks_MODEL?=Pixel 7 Pro \
+    persist.sys.pihooks_FINGERPRINT?=google/cheetah_beta/cheetah:16/BP22.250325.007/13352765:user/release-keys \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP22.250325.007 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-04-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 # Evolution X packages
